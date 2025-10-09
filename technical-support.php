ac3a4226d842c70b964e4a6b7c87a5c348908d8d
gigalabz <thegigalabz@gmail.com>
2025-10-09 11:51:49 +0500

    technical support

---
+++ technical-support.php
@@ -0,0 +1,36 @@
+<?php
+    include 'header-en.php';
+?>
+
+
+</div><div id="ajax-content-wrap">
+<div class="container-wrap">
+<div class="container main-content">
+<div class="row">
+<div id="fws_68d162ff7b13c" data-column-margin="default" data-midnight="dark" class="wpb_row vc_row-fluid vc_row top-level" style="padding-top: 0px; padding-bottom: 0px;"><div class="row-bg-wrap" data-bg-animation="none" data-bg-overlay="false"><div class="inner-wrap"><div class="row-bg viewport-desktop"></div></div></div><div class="row_col_wrap_12 col span_12 dark left">
+<div class="vc_col-sm-12 wpb_column column_container vc_column_container col no-extra-padding inherit_tablet inherit_phone" data-padding-pos="all" data-has-bg-color="false" data-bg-color="" data-bg-opacity="1" data-animation="" data-delay="0">
+<div class="vc_column-inner">
+<div class="wpb_wrapper">
+<div class="divider-wrap height_phone_120px" data-alignment="default"><div style="height: 200px;" class="divider"></div></div><div id="fws_68d162ff7d9dc" data-midnight="" data-column-margin="default" class="wpb_row vc_row-fluid vc_row inner_row"><div class="row-bg-wrap"> <div class="row-bg"></div></div><div class="row_col_wrap_12_inner col span_12 left">
+<div class="vc_col-sm-6 wpb_column column_container vc_column_container col child_column no-extra-padding inherit_tablet inherit_phone" data-padding-pos="all" data-has-bg-color="false" data-bg-color="" data-bg-opacity="1" data-animation="" data-delay="0">
+<div class="vc_column-inner">
+<div class="wpb_wrapper">
+<div class="nectar-highlighted-text" data-style="full_text" data-exp="default" data-using-custom-color="false" data-animation-delay="false" data-color="" data-color-gradient=""><span class="title-green">TECHNICAL<br /> SUPPORT</span></div><div class="wpb_text_column wpb_content_element">
+<div class="wpb_wrapper"> <h2 class="sub-minuscula">We want to be part of the growers’ team to take our varieties’ performance to the highest level.</h2></div></div><div class="wpb_text_column wpb_content_element">
+<div class="wpb_wrapper"> <p>Our agronomists use a farm management information system to help growers collecting data and translate them into key information to improve their crops’ performance.</p> <p>We want to turn the trust they place in us into real benefits, providing technical advice and our know-how throughout the growing process.</p> <p>Our commitment is to maintain a fluent, constant and transparent communication through all our channels, being there when our customers need us.</p></div></div></div></div></div><div class="vc_col-sm-6 wpb_column column_container vc_column_container col child_column has-animation no-extra-padding inherit_tablet inherit_phone mobile-disable-entrance-animation" data-padding-pos="all" data-has-bg-color="false" data-bg-color="" data-bg-opacity="1" data-animation="fade-in-from-right" data-delay="0">
+<div class="vc_column-inner">
+<div class="wpb_wrapper">
+<div class="img-with-aniamtion-wrap" data-max-width="100%" data-max-width-mobile="default" data-shadow="none" data-animation="fade-in">
+<div class="inner">
+<div class="hover-wrap" data-hover-animation="zoom"> 
+<div class="hover-wrap-inner"> <img fetchpriority="high" decoding="async" class="img-with-animation skip-lazy nectar-lazy" data-delay="0" height="535" width="613" data-animation="fade-in" data-nectar-img-src="wp-content/uploads/2022/02/Grupo-980.jpg" src="data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%20613%20535'%2F%3E" alt="" data-nectar-img-srcset="wp-content/uploads/2022/02/Grupo-980.jpg 613w, wp-content/uploads/2022/02/Grupo-980-300x262.jpg 300w, wp-content/uploads/2022/02/Grupo-980-600x524.jpg 600w" sizes="(max-width: 613px) 100vw, 613px"/></div></div></div></div></div></div></div></div></div></div></div></div></div></div><div id="fws_68d162ff80035" data-column-margin="default" data-midnight="dark" class="wpb_row vc_row-fluid vc_row" style="padding-top: 0px; padding-bottom: 0px;"><div class="row-bg-wrap" data-bg-animation="none" data-bg-overlay="false"><div class="inner-wrap"><div class="row-bg viewport-desktop"></div></div></div><div class="row_col_wrap_12 col span_12 dark left">
+<div class="vc_col-sm-12 wpb_column column_container vc_column_container col centered-text no-extra-padding inherit_tablet inherit_phone" data-padding-pos="all" data-has-bg-color="false" data-bg-color="" data-bg-opacity="1" data-animation="" data-delay="0">
+<div class="vc_column-inner">
+<div class="wpb_wrapper">
+<div class="divider-wrap" data-alignment="default"><div style="height: 55px;" class="divider"></div></div><a class="nectar-button small regular extra-color-1 regular-button pl-yellow-button" href="varieties" data-color-override="false" data-hover-color-override="false" data-hover-text-color-override="#fff"><span>Check our varieties</span></a></div></div></div></div></div></div></div></div><div id="footer-outer" data-midnight="light" data-cols="3" data-custom-color="true" data-disable-copyright="true" data-matching-section-color="false" data-copyright-line="false" data-using-bg-img="false" data-bg-img-overlay="0.8" data-full-width="false" data-using-widget-area="true" data-link-hover="default">
+<div id="footer-widgets" data-has-widgets="true" data-cols="3">
+
+
+<?php
+    include 'footer-en.php';
+?>
