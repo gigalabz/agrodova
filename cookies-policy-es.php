d5560224cbb509ef48dd823e3c273b227b98f1e6
gigalabz <thegigalabz@gmail.com>
2025-10-12 14:15:49 +0500

    cookie es

---
+++ cookies-policy-es.php
@@ -0,0 +1,121 @@
+<?php include 'header-es.php'; ?>
+
+
+
+
+</div><div id="ajax-content-wrap">
+<div class="container-wrap">
+<div class="container main-content">
+<div class="row">
+<div id="legal" data-column-margin="default" data-midnight="dark" class="wpb_row vc_row-fluid vc_row top-level legal-pages" style="padding-top: 0px; padding-bottom: 0px;"><div class="row-bg-wrap" data-bg-animation="none" data-bg-overlay="false"><div class="inner-wrap"><div class="row-bg viewport-desktop"></div></div></div><div class="row_col_wrap_12 col span_12 dark left">
+<div class="vc_col-sm-12 wpb_column column_container vc_column_container col no-extra-padding inherit_tablet inherit_phone" data-padding-pos="all" data-has-bg-color="false" data-bg-color="" data-bg-opacity="1" data-animation="" data-delay="0">
+<div class="vc_column-inner">
+<div class="wpb_wrapper">
+<div class="wpb_text_column wpb_content_element">
+<div class="wpb_wrapper">
+<h1>POLÍTICA<br /> DE COOKIES</h1>
+<p>Plantas de Navarra, S.A. (en adelante, Agrodova) le informa de que este sitio web utiliza cookies propias y de terceros para analizar las visitas, el tráfico y la navegación en nuestro sitio web, así como para gestionar el registro y mantener la sesión en la sección de Empleo del sitio.</p>
+<p>Cualquier acción que implique la continuidad en la navegación por el sitio web, o la aceptación del mismo, supondrá la autorización para el uso del primer grupo de cookies. Las cookies relacionadas con la sección de Uso de la Web son esenciales para su correcto funcionamiento. En cualquier caso, pueden eliminarse en cualquier momento siguiendo los parámetros de su navegador.<br /> La aceptación de esta Política implica que el usuario ha sido informado de manera clara y completa sobre el uso de los dispositivos de almacenamiento y recuperación de datos (cookies) y que Agrodova cuenta con el consentimiento del usuario para utilizarlas en los términos establecidos por la normativa vigente.</p>
+<h2>1.- ¿Qué es una cookie y para qué sirve?</h2>
+<p>Una cookie es un archivo que se descarga en su ordenador, smartphone, tableta, etc., al acceder a determinadas páginas web. Las cookies permiten a una página web, entre otras cosas, almacenar y recuperar información sobre los hábitos de navegación de un usuario o de su equipo y, dependiendo de la información que contengan y de la forma en que utilice su equipo, pueden utilizarse para reconocer al usuario.</p>
+<h2>2.- ¿Clasificación de las cookies?</h2>
+<p>Las cookies pueden clasificarse de la siguiente manera:</p>
+<p>A. Según la entidad que las gestione:</p>
+<p>a) Cookies propias: Son aquellas que se envían a su equipo desde nuestros propios servidores o dominios y desde los que prestamos el servicio que usted solicita.</p>
+<p>b) Cookies de terceros: Son aquellas que se envían a su equipo desde un equipo o dominio que no es gestionado por nosotros, sino por otra entidad colaboradora. Por ejemplo, las utilizadas por redes sociales.</p>
+<p>B. Según el plazo de tiempo que permanecen activadas:</p>
+<p>a) Cookies de sesión: Son cookies temporales que permanecen en el archivo de cookies de su navegador hasta que abandona el sitio web, de modo que ninguna queda registrada en el disco duro de su ordenador.</p>
+<p>b) Cookies persistentes: Se almacenan en el disco duro y nuestra web las lee cada vez que usted vuelve a visitarla. Una cookie permanente tiene una fecha de caducidad; la cookie dejará de funcionar después de esa fecha.</p>
+<p>C. Según su finalidad:</p>
+<p>a) Cookies técnicas: Son necesarias para la navegación y para la ejecución de determinadas funciones de nuestro sitio web.<br />
+b) Cookies de personalización: Permiten acceder al servicio con características predefinidas según una serie de criterios, como el idioma, el tipo de navegador utilizado para acceder al servicio, la configuración regional desde la que se accede, etc.<br />
+c) Cookies de análisis: Permiten cuantificar el número de usuarios y así realizar la medición y análisis estadístico del uso que hacen los usuarios de los servicios ofrecidos.<br />
+d) Cookies publicitarias: Permiten la gestión, de la forma más eficaz posible, de los espacios publicitarios que puedan incluirse en una página web.</p>
+<h2 id="cookie-we-use">3.- Cookies que utilizamos en este sitio web</h2>
+<p>De acuerdo con las directrices de la Agencia Española de Protección de Datos, procedemos a detallar el uso de cookies en este sitio web con el fin de informarle de forma precisa.</p>
+</div></div><div class="wpb_raw_code wpb_content_element wpb_raw_html">
+<div class="wpb_wrapper">
+<section>
+<table class="Table_Desktop">
+<tr>
+<th id="cookie">Nombre de la cookie</th>
+<th id="Purpose">Finalidad</th>
+<th id="dutation">Duración</th>
+<th id="Purpose">Responsable</th>
+</tr>
+<tr>
+<td>_gat_gtag_UA_97945334_1</td>
+<td>Análisis, tráfico y navegación web, para limitar el porcentaje de solicitudes.</td>
+<td>1 minuto</td>
+<td>Cookie de terceros (Google Analytics)</td>
+</tr>
+<tr>
+<td>_ga</td>
+<td>Cookies de análisis, tráfico y navegación web, para distinguir a los usuarios.</td>
+<td>2 años</td>
+<td>Cookie de terceros (Google Analytics)</td>
+</tr>
+<tr>
+<td>_gid</td>
+<td>Esta cookie es establecida por Google Analytics. Almacena y actualiza un valor único para cada página visitada y se utiliza para contar y rastrear las vistas de página.</td>
+<td>1 día</td>
+<td>Cookie de terceros (Google Analytics)</td>
+</tr>
+<tr>
+<td>cookie_notice_accepted</td>
+<td>Cookie técnica que se genera cuando se hace clic en "Ok" en el aviso emergente sobre la política de cookies para gestionar el consentimiento.</td>
+<td>1 mes</td>
+<td>Cookie propia</td>
+</tr>
+<tr>
+<td>wp-wpml_current_language</td>
+<td>Almacena el idioma actual.</td>
+<td>Sesión</td>
+<td>Cookie propia, funcional</td>
+</tr>
+<tr>
+<td>_icl_visitor_lang_js</td>
+<td>Almacena el idioma redirigido. Esta cookie se habilita para todos los visitantes del sitio si se utiliza la función de redirección de idioma del navegador.</td>
+<td>1 día</td>
+<td>Cookie propia, funcional</td>
+</tr>
+<tr>
+<td>apbct_page_hits</td>
+<td>Cookie funcional colocada por CleanTalk Spam Protect para prevenir el spam en este sitio web. Su función exacta no está completamente clara. Almacena el número de páginas visitadas.</td>
+<td>Sesión</td>
+<td>Cookie de terceros (Clean Talk)</td>
+</tr>
+<tr>
+<td>apbct_site_landing_ts</td>
+<td>Cookie funcional colocada por CleanTalk Spam Protect. Su contenido es un número cuyo significado no está claro.</td>
+<td>Sesión</td>
+<td>Cookie de terceros (Clean Talk)</td>
+</tr>
+<tr>
+<td>apbct_cookies_test</td>
+<td>Se utiliza para comprobar si se pueden colocar cookies.</td>
+<td>Sesión</td>
+<td>Cookie de terceros (Clean Talk)</td>
+</tr>
+<tr>
+<td>apbct_timestamp</td>
+<td>Se utiliza para comprobar si se pueden colocar cookies.</td>
+<td>Sesión</td>
+<td>Cookie de terceros (Clean Talk)</td>
+</tr>
+<tr>
+<td>ct_checkjs, ct_checkjs, apbct_pixel_url, apbct_site_referer, apbct_urls, ct_fkp_timestamp, ct_timezone, ct_checked_emails, ct_pointer_data, ct_screen_info, ct_ps_timestamp, ct_sfw_pass_key</td>
+<td>Cookies funcionales utilizadas por Clean Talk.</td>
+<td>Sesión</td>
+<td>Cookie de terceros (Clean Talk)</td>
+</tr>
+</table>
+</section>
+</div></div><div class="wpb_text_column wpb_content_element">
+<div class="wpb_wrapper">
+<p>De acuerdo con los términos y condiciones de Google Analytics, le informamos que este servicio de análisis web es proporcionado por Google, Inc. (en adelante, Google), una empresa de Delaware con sede principal en 1600 Amphitheatre Parkway, Mountain View (California), CA 94043, Estados Unidos. Google Analytics utiliza sus propias cookies, indicadas en los puntos anteriores, para ayudar al sitio web a analizar el uso que hacen los usuarios del mismo. La información generada por la cookie sobre su uso del sitio web (incluida su dirección IP) será transmitida directamente a y almacenada por Google en servidores situados en Estados Unidos.</p>
+<p>Google utilizará estos datos, las páginas de procedencia y búsquedas, los datos de localización y otros hábitos de navegación, para recopilar información sobre el uso del sitio web, elaborar informes de actividad y prestar otros servicios relacionados con el sitio web y el uso de Internet. El uso de esta información es responsabilidad exclusiva de Google, que establece en sus condiciones que puede ser transmitida a terceros cuando la ley lo requiera, o cuando dichos terceros procesen la información en nombre de Google.</p>
+</div></div></div></div></div></div></div>
+
+
+<?php include 'footer-es.php'; ?>
